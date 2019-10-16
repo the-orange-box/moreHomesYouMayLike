dd75089b75b8ef1215eb894652aad4ef0cd29165
Gal Weinstock <gal.weinstock@post.idc.ac.il>
2019-10-16 14:37:42 -0700

    Install jest and @babel/polyfill packages

--- test/index.test.js
+++ test/index.test.js
@@ -8,7 +8,7 @@ const {shallow} = require('enzyme');
 describe('Top react component unit testing', () => {
   describe('Rendering test', () => {
     test('It should render react component (the whole page)', () => {
-      let tester = shallow(<App />);
+      const wrapper = shallow(<App />);
     });
   });
 });
