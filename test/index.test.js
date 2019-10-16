86288b8b455f69d0cc13ad1ecf0750b19600df71
Gal Weinstock <gal.weinstock@post.idc.ac.il>
2019-10-16 13:58:38 -0700

    Create enzyme config file

--- test/index.test.js
+++ test/index.test.js
@@ -1,12 +1,14 @@
-import indexApp from '../client/src/index.jsx';
-import React from 'react';
-import {shallow} from enzyme;
+// import indexApp from '../client/src/index.jsx';
+// import React from 'react';
+// import {shallow} from enzyme;
+const indexApp = require('../client/src/index.jsx');
+const React = require('react');
+const {shallow} = require('enzyme');
 
 describe('Top react component unit testing', () => {
   describe('Rendering test', () => {
     test('It should render react component (the whole page)', () => {
       let tester = shallow(<App />);
-
     });
   });
 });
